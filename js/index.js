be6b106995b678bc06f479ee98693a6ccf438618
Julian Perez <julianperez620@gmail.com>
2023-12-07 22:56:19 -0300

    new changes

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ steps:
 
 //Enlasarme al DOM
 
-const URLPn = "js/newInStore.json";
-const URL = "js/store.json";
+const URL2= "js/store.json";
 const cart = [];
 const cartNew = [];
 const newInStore = document.querySelector(".newIn");
+
 // const cardError = document.querySelector(".cartError");
 
 // store
@@ -41,7 +41,7 @@ function createCard(products) {
 //   }
 // }
 
-function displayProductsInNewIn() {
+function displayProducts() {
   newInStore.innerHTML = "";
 
   if (cartNew.length > 0) {
@@ -51,40 +51,134 @@ function displayProductsInNewIn() {
   }
 }
 
-
-
-function productSelectedOfNewIn() {
+function productSelected() {
   const buttonsBuy = document.querySelectorAll("button.purchase");
   buttonsBuy.forEach((button) => {
     button.addEventListener("click", (event) => {
       Toastify({
-
         text: "Added Product",
         style: {
           background: "linear-gradient(to right, #8ADAB2, #DF826C)",
         },
-        duration: 1000
-        
-        }).showToast();
+        duration: 1000,
+      }).showToast();
       const id = parseFloat(event.target.id);
       const selected = cartNew.find((p) => p.id === id);
-
       cart.push(selected);
       localStorage.setItem("newCart", JSON.stringify(cart));
     });
   });
 }
 
-
-function obtainProducts() {
-  fetch(URLPn)
+//obtaining products the backEnd
+function obtainProductsCartNew() {
+  fetch(URL2)
     .then((answer) => answer.json())
     .then((data) => {
       cartNew.push(...data);
-      displayProductsInNewIn();
-      productSelectedOfNewIn();
+      displayProducts();
+      productSelected();
     });
 }
 
-obtainProducts();
-//obtaining products the backEnd
+obtainProductsCartNew();
+
+//email footer
+
+
+function obtainDataInput() {
+  const sendEmail = document.querySelector("#submit");
+  const input = document.querySelector(".inputE");
+
+  function showMessageSuccess() {
+    Swal.fire({
+      title: "Good job!",
+      text: "Your information was sended correctly",
+      icon: "success"
+    });
+  }
+
+  function showMessageError(){
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "The field is empty!",
+    });
+  };
+
+  function handleClick() {
+    if (input.value.trim() !== "") {
+      showMessageSuccess();
+    } else {
+      showMessageError();
+    }
+  }
+
+  sendEmail.addEventListener("click", handleClick);
+}
+
+obtainDataInput()
+
+
+//show cart 
+
+
+ const listCart = document.querySelector("#counter");
+
+function createCardShowProducts(show) {
+  return `
+  <table class="table">
+  <thead>
+      <tr>
+          <th>image</th>
+          <th>tittle</th>
+          <th>price</th>
+          </tr>
+  </thead>
+
+  <tbody>
+      <tr>
+          <th><img src="${show.image}" alt="img"></th>
+          <th>${show.title}</th>
+          <th class="price">$${show.price}</th>
+      </tr>
+  </tbody>
+  <tfoot>
+      <tr>
+          <th id="total">total:${updateTotal()}</th>
+          <th></th>
+          <th></th>
+
+      </tr>
+  </tfoot>
+
+  
+</table>
+    `;
+}
+
+
+  const stored = localStorage.getItem("newCart");
+  const resultStored = JSON.parse(stored);
+  const buttonFinalizePurchase = document.querySelector("#finalizePurchase");
+
+
+function calculateTotal() {
+  const total = resultStored.reduce((acc, product) => acc + product.price, 0);
+  return total;
+}
+
+function updateTotal() {
+  const totalElement = document.getElementById('total');
+  const total = calculateTotal();
+  totalElement.textContent = `total: $${total}`;
+}
+
+
+
+
+ 
+
+
+
+
